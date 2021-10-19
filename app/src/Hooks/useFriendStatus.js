3f1af5c99180d3f54ccb2af6a862117e0add03ce
bodeby <thorbensen@gmail.com>
2021-10-19 10:14:24 +0200

    test

---
+++ app/src/Hooks/useFriendStatus.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+function useFriendStatus(friendID) {  const [isOnline, setIsOnline] = useState(null);
+
+  function handleStatusChange(status) {
+    setIsOnline(status.isOnline);
+  }
+
+  useEffect(() => {
+    ChatAPI.subscribeToFriendStatus(friendID, handleStatusChange);
+    return () => {
+      ChatAPI.unsubscribeFromFriendStatus(friendID, handleStatusChange);
+    };
+  });
+
+  return isOnline;
+}
